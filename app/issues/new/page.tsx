58d4de7855e3892486a90d85c9a77f8b1dc0abe4
Abu Jayed <aj.webdevlopment@gmail.com>
2023-10-17 14:16:30 +0600

    disable "ssr" to load markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,24 +1,20 @@
 "use client";
-import React from "react";
-import { useState } from "react";
-import {
-  Button,
-  Callout,
-  CalloutRoot,
-  Text,
-  TextArea,
-  TextField,
-} from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
-import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 // interface IssueForm {
 //   title: string;
@@ -38,17 +34,17 @@ const NewIssuePage = () => {
   });
   const router = useRouter();
   const [error, setError] = useState("");
-  const [isSubmitting,setSubmitting] = useState(false);
+  const [isSubmitting, setSubmitting] = useState(false);
   const onSubmit = handleSubmit(async (data) => {
     try {
-      setSubmitting(true)
+      setSubmitting(true);
       await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
-      setSubmitting(false)
+      setSubmitting(false);
       setError("unexpected error");
     }
-  })
+  });
   return (
     <div className="max-w-xl">
       <div className="bg-white">
@@ -58,10 +54,7 @@ const NewIssuePage = () => {
           </Callout.Root>
         )}
       </div>
-      <form
-        className="space-y-3"
-        onSubmit={onSubmit}
-      >
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input
             placeholder="Title"
@@ -77,7 +70,9 @@ const NewIssuePage = () => {
           )}
         ></Controller>
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner></Spinner>} </Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner></Spinner>}{" "}
+        </Button>
       </form>
     </div>
   );
