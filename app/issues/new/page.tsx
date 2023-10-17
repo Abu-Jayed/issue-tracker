3d3274d592f76081cf4e9cc5aa795c8ad908f825
Abu Jayed <aj.webdevlopment@gmail.com>
2023-10-17 07:41:41 +0600

    spinner component created.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -18,6 +18,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 // interface IssueForm {
 //   title: string;
@@ -37,6 +38,8 @@ const NewIssuePage = () => {
   });
   const router = useRouter();
   const [error, setError] = useState("");
+  const [isSubmitting,setSubmitting] = useState(false);
+  
   return (
     <div className="max-w-xl">
       <div className="bg-white">
@@ -50,9 +53,11 @@ const NewIssuePage = () => {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubmitting(true)
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
+            setSubmitting(false)
             setError("unexpected error");
           }
         })}
@@ -72,7 +77,7 @@ const NewIssuePage = () => {
           )}
         ></Controller>
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner></Spinner>} </Button>
       </form>
     </div>
   );
