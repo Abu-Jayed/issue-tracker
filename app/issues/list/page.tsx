a2d4625090dcaa251f6d84bd3b1fc0ed559e2196
Abu Jayed <aj.webdevlopment@gmail.com>
2023-10-19 09:00:45 +0600

    issues filter done.

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,11 +2,23 @@ import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import delay from "delay";
 import IssueActions from "./IssueActions";
-import {IssueStatusBadge,Link} from "@/app/components/index"
+import { IssueStatusBadge, Link } from "@/app/components/index";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
-  await delay(1500)
+const IssuesPage = async ({
+  searchParams,
+}: {
+  searchParams: { status: Status };
+}) => {
+  const statuses = Object.values(Status)
+  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
+
+  const issues = await prisma.issue.findMany({
+    where:{
+      status
+    }
+  });
+  await delay(1500);
   return (
     <div>
       <IssueActions></IssueActions>
@@ -26,9 +38,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>
-                {issue.title}
-                </Link>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="md:hidden block ">
                   <IssueStatusBadge status={issue.status}></IssueStatusBadge>{" "}
                 </div>{" "}
