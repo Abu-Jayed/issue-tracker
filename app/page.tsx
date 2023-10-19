19bcd3f5700e0386d1ace4d4405e72c2f179d6a3
Abu Jayed <aj.webdevlopment@gmail.com>
2023-10-19 11:25:15 +0600

    implement pagination. Pagination button working.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <>
       <div>This is our home</div>
-      <Pagination itemCount={100} pageSize={10} currentPage={2}></Pagination>
+      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}></Pagination>
     </>
   );
 }
