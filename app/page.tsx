55b088d0cc081c97f5b366510c3e19631855949a
Abu Jayed <aj.webdevlopment@gmail.com>
2023-10-19 13:59:09 +0600

    Issue summary component created.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import LatestIssues from "./LatestIssues";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+
   return (
     <>
-      <LatestIssues></LatestIssues>
+      <IssueSummary
+        open={open}
+        closed={closed}
+        inProgress={inProgress}
+      ></IssueSummary>
     </>
   );
 }
